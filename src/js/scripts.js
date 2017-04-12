8c517ff7614056be697774f059b837cf7da52f7b
Dyego Cruz <dyegocruz@gmail.com>
2017-04-12 08:46:21 -0300

    Conclusion integration with instagram and started implementing of sidebar.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,6 +21,29 @@ $( document ).ready(function() {
         //}
     });
 
+    var token = '30666716.1b6f424.e88abbf4a7434ca0acd1f1b530a091e4', // learn how to obtain it below
+        userid = '30666716', // User ID - Modo Meu - get it in source HTML of your Instagram profile or look at the next example :)
+        num_photos = 7; // how much photos do you want to get*/
+ 
+    $.ajax({
+        url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+        dataType: 'jsonp',
+        type: 'GET',
+        data: {access_token: token, count: num_photos},
+        success: function(data){            
+            for( x in data.data ){         
+                $('.feed-instagram ul').append('<li><img class="photo-instagram" src="'+data.data[x].images.thumbnail.url+'"></li>');
+                //$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+                // data.data[x].images.thumbnail.url - URL of image 150х150
+                // data.data[x].images.standard_resolution.url - URL of image 612х612
+                // data.data[x].link - Instagram post URL 
+            }
+        },
+        error: function(data){
+            console.log(data); // send the error notifications to console
+        }
+    });
+
 });
 
 $(window).scroll(function(){
