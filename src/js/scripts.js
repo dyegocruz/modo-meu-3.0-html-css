1012fb177aa243f657b28688fb09668e9df92d0a
Dyego Alberto da Cruz barbosa <u200136@marquise.com.br>
2017-04-12 15:46:54 -0300

    Implementing of new sidebar completed.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -25,24 +25,24 @@ $( document ).ready(function() {
         userid = '30666716', // User ID - Modo Meu - get it in source HTML of your Instagram profile or look at the next example :)
         num_photos = 7; // how much photos do you want to get*/
  
-    $.ajax({
-        url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
-        dataType: 'jsonp',
-        type: 'GET',
-        data: {access_token: token, count: num_photos},
-        success: function(data){            
-            for( x in data.data ){         
-                $('.feed-instagram ul').append('<li><img class="photo-instagram" src="'+data.data[x].images.thumbnail.url+'"></li>');
-                //$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-                // data.data[x].images.thumbnail.url - URL of image 150х150
-                // data.data[x].images.standard_resolution.url - URL of image 612х612
-                // data.data[x].link - Instagram post URL 
-            }
-        },
-        error: function(data){
-            console.log(data); // send the error notifications to console
-        }
-    });
+    // $.ajax({
+    //     url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+    //     dataType: 'jsonp',
+    //     type: 'GET',
+    //     data: {access_token: token, count: num_photos},
+    //     success: function(data){            
+    //         for( x in data.data ){         
+    //             $('.feed-instagram ul').append('<li><img class="photo-instagram" src="'+data.data[x].images.thumbnail.url+'"></li>');
+    //             //$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+    //             // data.data[x].images.thumbnail.url - URL of image 150х150
+    //             // data.data[x].images.standard_resolution.url - URL of image 612х612
+    //             // data.data[x].link - Instagram post URL 
+    //         }
+    //     },
+    //     error: function(data){
+    //         console.log(data); // send the error notifications to console
+    //     }
+    // });
 
 });
 
