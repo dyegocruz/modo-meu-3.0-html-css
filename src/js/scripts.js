aabf8f815b9576532e85447e23b26a546212af0a
Dyego Cruz <dyegocruz@gmail.com>
2018-03-15 17:20:17 -0300

    Adjusting header highlights.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,58 +1,60 @@
 $( document ).ready(function() {
 
-    $('.menu-category li').hover(
-        function() {        
-            
-            var newOffSet = ($('.menu-category').offset().left - $(this).find('a').first().offset().left);            
-            if (newOffSet < 0) {
-                $(this).find('.submenu-container').css('left',newOffSet);
-            }
+  $('.highlight-home-first').css('height',$('.highlight-home-secondaries').innerHeight()-10);  
 
-            $(this).find('.submenu-container').css('width',$('.menu-category').width());
-            $(this).find('.submenu-container').fadeIn();
-            $(this).find('.submenu-container').find('.child-row:eq(0)').show();
-    }, function() { 
-        $(this).find('.submenu-container').fadeOut();
-    });
+  $('.menu-category li').hover(
+      function() {        
+          
+          var newOffSet = ($('.menu-category').offset().left - $(this).find('a').first().offset().left);            
+          if (newOffSet < 0) {
+              $(this).find('.submenu-container').css('left',newOffSet);
+          }
 
-    $('.submenu li').hover(function() {
-        $(this).parent().find('li').removeClass('active');
-        $(this).addClass('active');
-		
-        var subMenuCatIndex = $(this).index();
-		
-		var thisCategoryChildren = $(this).parent().parent().find('.category-children');		
-		$(thisCategoryChildren).find('.child-row').hide();
-		$(thisCategoryChildren).find('.child-row:eq(' + subMenuCatIndex + ')').show();	
-    });
+          $(this).find('.submenu-container').css('width',$('.menu-category').width());
+          $(this).find('.submenu-container').fadeIn();
+          $(this).find('.submenu-container').find('.child-row:eq(0)').show();
+  }, function() { 
+      $(this).find('.submenu-container').fadeOut();
+  });
 
-    $('.button-back-to-top').click( function(e) {
-        $('html, body').animate({scrollTop: 0}, 1000);
-        return false;
-    });
+  $('.submenu li').hover(function() {
+      $(this).parent().find('li').removeClass('active');
+      $(this).addClass('active');
+  
+      var subMenuCatIndex = $(this).index();
+  
+  var thisCategoryChildren = $(this).parent().parent().find('.category-children');		
+  $(thisCategoryChildren).find('.child-row').hide();
+  $(thisCategoryChildren).find('.child-row:eq(' + subMenuCatIndex + ')').show();	
+  });
 
-    var token = '30666716.1b6f424.e88abbf4a7434ca0acd1f1b530a091e4', // learn how to obtain it below
-        userid = '30666716', // User ID - Modo Meu - get it in source HTML of your Instagram profile or look at the next example :)
-        num_photos = 7; // how much photos do you want to get*/
- 
-    // $.ajax({
-    //     url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
-    //     dataType: 'jsonp',
-    //     type: 'GET',
-    //     data: {access_token: token, count: num_photos},
-    //     success: function(data){            
-    //         for( x in data.data ){          
-    //             $('.feed-instagram ul').append('<li><img class="photo-instagram" src="'+data.data[x].images.thumbnail.url+'"></li>');
-    //             //$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-    //             // data.data[x].images.thumbnail.url - URL of image 150х150
-    //             // data.data[x].images.standard_resolution.url - URL of image 612х612
-    //             // data.data[x].link - Instagram post URL 
-    //         }
-    //     },
-    //     error: function(data){
-    //         console.log(data); // send the error notifications to console
-    //     }
-    // });
+  $('.button-back-to-top').click( function(e) {
+      $('html, body').animate({scrollTop: 0}, 1000);
+      return false;
+  });
+
+  var token = '30666716.1b6f424.e88abbf4a7434ca0acd1f1b530a091e4', // learn how to obtain it below
+      userid = '30666716', // User ID - Modo Meu - get it in source HTML of your Instagram profile or look at the next example :)
+      num_photos = 7; // how much photos do you want to get*/
+
+  // $.ajax({
+  //     url: 'https://api.instagram.com/v1/users/' + userid + '/media/recent', // or /users/self/media/recent for Sandbox
+  //     dataType: 'jsonp',
+  //     type: 'GET',
+  //     data: {access_token: token, count: num_photos},
+  //     success: function(data){            
+  //         for( x in data.data ){          
+  //             $('.feed-instagram ul').append('<li><img class="photo-instagram" src="'+data.data[x].images.thumbnail.url+'"></li>');
+  //             //$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+  //             // data.data[x].images.thumbnail.url - URL of image 150х150
+  //             // data.data[x].images.standard_resolution.url - URL of image 612х612
+  //             // data.data[x].link - Instagram post URL 
+  //         }
+  //     },
+  //     error: function(data){
+  //         console.log(data); // send the error notifications to console
+  //     }
+  // });
 
 });
 
