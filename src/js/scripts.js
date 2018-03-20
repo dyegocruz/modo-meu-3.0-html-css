5f4d291f7830b45cce9114145557fe4b650eaf1b
Dyego Cruz <dyegocruz@gmail.com>
2018-03-19 23:19:25 -0300

    Conclusion of videos list and full containers.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 
   $('.highlight-home-first').css('height',$('.highlight-home-secondaries').innerHeight()-10);
   $('.highlight-home-secondaries .highlight-home-text').css('height',$('.highlight-home-secondary-img img').innerHeight());
+  $('.item-video .item-text').css('height',$('.item-image img').innerHeight());
 
   $('.menu-category li').hover(
       function() {        
